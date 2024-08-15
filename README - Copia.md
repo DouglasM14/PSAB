cd90675308383eba72c6f0fef74d302dd6953b5e
link_devote <160704579+EduardoNascimentoSoares@users.noreply.github.com>
2024-08-14 21:34:19 -0300

    1.1.1
    consertando erro do commit com o branch eduardo

---
+++ README - Copia.md
@@ -0,0 +1,2 @@
+# PSAB
+ Prototipo de um Sistema de Agendamento para Barbearia
---
+++ public - Copia/index2.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PSAB</title>
+</head>
+
+<body>
+    <header>
+        <h1>PSAB - index</h1>
+    </header>
+
+    <main>
+        <a href="login.php">Entra na sua conta</a>
+    </main>
+
+    <Footer>
+        <p>Site desenvolvido por Douglas & Eduardo</p>
+        <p>Etec de Heliopolis 2024</p>
+    </Footer>
+</body>
+
+</html>
